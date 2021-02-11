04588c61dd6d6bf236d6086e93128be666b3846a
rflnhdyt626 <77946109+rflnhdyt626@users.noreply.github.com>
2021-02-11 19:17:18 +0700

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -6,12 +6,9 @@ Syarat dan Ketentuan Bot *KAMI*
 3. Kami tidak menyimpan gambar, video, file, audio, dan dokumen yang anda kirim
 4. Kami tidak akan pernah meminta anda untuk memberikan informasi pribadi
 5. Jika menemukan Bug/Error silahkan langsung lapor ke Owner bot
-6. Juga kalau kalian ingin buat TUTORIAL BOT Langsung Lewat
-     ➥ YouTube: https://www.youtube.com/c/DrawlNag
 6. Apapun yang anda perintah pada bot ini, KAMI TIDAK AKAN BERTANGGUNG JAWAB!
 
 Thanks !
-2021©Drawl Nag
 `.trim(), m)
 }
 handler.help = ['rules']
