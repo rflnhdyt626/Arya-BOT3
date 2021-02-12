a3eff69c48bc97a264d3051c6af02098a2dc19dd
rflnhdyt626 <77946109+rflnhdyt626@users.noreply.github.com>
2021-02-12 13:57:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('DRAWL NAG\nWhatsApp Bot', {
+CFonts.say('RFL BOT\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
