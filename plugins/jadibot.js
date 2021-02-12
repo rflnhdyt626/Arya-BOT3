851f2c4a456942472ece00024157c35906e84c8d
rflnhdyt626 <77946109+rflnhdyt626@users.noreply.github.com>
2021-02-12 14:56:06 +0700

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -50,7 +50,7 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
 handler.help = ['jadibot']
 handler.tags = ['jadibot']
 handler.command = /^jadibot$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
