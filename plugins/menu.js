405ef60b5bbae6d098fcb1fdf3b02009eaa60c13
rflnhdyt626 <77946109+rflnhdyt626@users.noreply.github.com>
2021-02-15 11:02:30 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -94,8 +94,6 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 
 ╭════•›「 Iklan 」
 ╿• BIKIN BOT WHATSAPP MURAH
-╿• SEWA BOT WHATSAPP MURAH
-╿• MASUKIN BOT KE GRUP 
 ╿• YOUTUBE PREMIUM 1 BULAN Rp 5.000
 ╿• YOUTUBE PREMIUM 1 TAHUN Rp 27.000
 ╿• Minat?
