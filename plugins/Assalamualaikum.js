5754432e3e9f044d00db75a9760492101505a1ef
rflnhdyt626 <77946109+rflnhdyt626@users.noreply.github.com>
2021-03-10 16:49:37 +0700

    Update Assalamualaikum.js

--- plugins/Assalamualaikum.js
+++ plugins/Assalamualaikum.js
@@ -1,9 +1,28 @@
+let fs = require ('fs')
+let path = require('path')
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  try {
+    let package = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json')))
+    let exp = global.DATABASE.data.users[m.sender].exp
+    let limit = global.DATABASE.data.users[m.sender].limit
+    let name = conn.getName(m.sender)
+    let d = new Date
+    let locale = 'id'
+    let gmt = new Date(0).getTime() - new Date('1 January 1970').getTime()
+    let weton = ['Pahing', 'Pon','Wage','Kliwon','Legi'][Math.floor(((d * 1) + gmt) / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `Waalaikumsalam`.trim(), m)
 }
 handler.help = ['Assalamualaikum']
 handler.tags = ['kepo']
-handler.command = /^(Assalamualaikum)$/i
+handler.command = /^(Assalamualaikum kak %name)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
